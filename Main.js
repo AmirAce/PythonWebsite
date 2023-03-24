8c61201e6b9351ec075c736402d704691a484d6f
AmirAce2 <58918353+AmirAce@users.noreply.github.com>
2023-03-24 14:30:56 -0400

    adding modules

---
+++ Main.js
@@ -0,0 +1,9 @@
+import { World } from './World/World.js';
+
+// create the main function
+function main() {
+// code to set up the World App will go here
+}
+
+// call main to start the app
+main();
---
+++ components/camera.js
@@ -0,0 +1,26 @@
+import { PerspectiveCamera } from 'three';
+
+function createCamera() {
+  const camera = new PerspectiveCamera(
+    35, // fov = Field Of View
+    1, // aspect ratio (dummy value)
+    0.1, // near clipping plane
+    100, // far clipping plane
+  );
+
+  // move the camera back so we can view the scene
+  camera.position.set(0, 0, 10);
+
+  return camera;
+}
+
+
+export { createCamera };
+
+// Create a camera
+const fov = 35; // AKA Field of View
+const aspect = container.clientWidth / container.clientHeight;
+const near = 0.1; // the near clipping plane
+const far = 100; // the far clipping plane
+
+const camera = new PerspectiveCamera(fov, aspect, near, far);
